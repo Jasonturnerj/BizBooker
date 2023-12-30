38e9105d9e21ba97b2be251d05299c9e4906276a
Jasonturnerj <jasonturner2350@gmail.com>
2023-12-29 17:23:08 -0800

    server prep

--- app.py
+++ app.py
@@ -14,18 +14,34 @@ from models import db, connect_db, Users, Business, Appointment
 
 CURR_USER_KEY = "curr_user"
 
-app = Flask(__name__)
-app.app_context().push()
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql://postgres:1122@localhost/bizbooker'))
+def create_app():
+    app = Flask(__name__)    
+    app.app_context().push()
+    # Get DB_URI from environ variable (useful for production/testing) or,
+    # if not set there, use development local db.
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL'))
+    # external link to be used when accesseing from outside of a render instance i.e. localhost
+    # 'postgresql://bizbooker_0hhh_user:UdqaHQxuyhCKvn7rI74sjeXMVE0BMKDR@dpg-cm7m1q6n7f5s73cdn210-a.oregon-postgres.render.com/bizbooker_0hhh'
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config['SQLALCHEMY_ECHO'] = False
+
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+    db.app_app(app)
+
+    app.register_blueprint()
+
+    return app
+
+app = create_app()
+    
 toolbar = DebugToolbarExtension(app)
+
 migrate = Migrate(app,db)
 
 @app.before_request
